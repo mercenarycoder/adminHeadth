49d5838a3f28b205b754b8f800e636f55b0b7a49
Manjeet <manjeetts869@gmail.com>
2021-07-17 18:15:37 +0530

    layout 1 of dashboard inserted

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,39 +1,47 @@
 import React from 'react';
 import './dashboard.scss';
 import Button2 from '../../utilities/button2/button2';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import AppsIcon from '@material-ui/icons/Apps';
+import Button from '../../utilities/button/Button';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PersonIcon from '@material-ui/icons/Person';
+import ClosedCaptionIcon from '@material-ui/icons/ClosedCaption';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 
 export default function Dashboard() {
     return (
-        <div
+        <div className="error_dialog"
             style={{
                 backgroundImage: `url(${process.env.PUBLIC_URL}assets/background_dashy.jpg)`,
-                
+
             }}
+        >
+            <div
             className="container_dashboard">
+            {/* Navigation code starts here */}
             <div className="dashboard_nav">
                 <div className="dashboard_nav_item dashboard_nav_item-1">
                     <img src={process.env.PUBLIC_URL + 'assets/3dlogo.png'} alt="company logo" className='register__logo-1' />
                 </div>
                 <div className="dashboard_nav_item dashboard_nav_item-2">
-                    <Button2 name="Overview"/>
+                    <Button2 name="Overview" />
                 </div>
                 <div className="dashboard_nav_item dashboard_nav_item-2">
                     {/* <a href="#" rel="noopener noreferrer">Prescriptions</a> */}
-                    <Button2 name="Prescriptions"/>
+                    <Button2 name="Prescriptions" />
                 </div>
                 <div className="dashboard_nav_item dashboard_nav_item-2">
-                    <Button2 name="Reports"/>
+                    <Button2 name="Reports" />
                     {/* <a href="#" rel="noopener noreferrer">Reports</a> */}
                 </div>
                 <div className="dashboard_nav_item dashboard_nav_item-3">
                     <div className="notification">
-                    <NotificationsIcon style={{fontSize:40 }} className="icon_dashboard"/>
-                    <span className="notification_text">0</span>
+                        <NotificationsIcon style={{ fontSize: 30 }} className="icon_dashboard" />
+                        <span className="notification_text">0</span>
                     </div>
                 </div>
                 <div className="dashboard_nav_item dashboard_nav_item-3">
@@ -41,41 +49,192 @@ export default function Dashboard() {
                     <span className="profile_text">Profile</span>
                 </div>
                 <div className="dashboard_nav_item dashboard_nav_item-3">
-                    <ExitToApp className="icon_dashboard" style={{fontSize:40 }}/>
+                    <ExitToApp className="icon_dashboard" style={{ fontSize: 30 }} />
                 </div>
             </div>
+            {/* Navigation code ends here */}
+            {/* dashboard sidebar code */}
             <div className="dashboard_sidebar">
-                <div className="sidebar_main">
-                    <div className="sidebar_main_items">
-                        <div className="sidebar_main_item sidebar_main_grey">Headth Studio</div>
-                        <div className="sidebar_main_item sidebar_main_icon">
-                            <AppsIcon className="icon_dashboard" style={{fontSize:30}}/>
-                            <div className="sidebar_main_white">Dashboard</div>
+                <div className="dashboard_sidebar_main">
+                    <div className="dashboard_sidebar_main_items">
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_grey">Headth Studio</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_icon">
+                            <AppsIcon className="icon_dashboard" style={{ fontSize: 30 }} />
+                            <div className="dashboard_sidebar_main_white">Dashboard</div>
                         </div>
-                        <div className="sidebar_main_item sidebar_main_grey">Headth Studio</div>
-                        <div className="sidebar_main_item sidebar_main_icon">
-                            <AppsIcon className="icon_dashboard" style={{fontSize:30}}/>
-                            <div className="sidebar_main_white">Dashboard</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_grey">Setting</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_icon">
+                            <PersonIcon className="icon_dashboard" style={{ fontSize: 30 }} />
+                            <div className="dashboard_sidebar_main_white">Profile Edit</div>
                         </div>
-                        <div className="sidebar_main_item sidebar_main_grey">Headth Studio</div>
-                        <div className="sidebar_main_item sidebar_main_icon">
-                            <AppsIcon className="icon_dashboard" style={{fontSize:30}}/>
-                            <div className="sidebar_main_white">Dashboard</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_grey">Membership Code</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_icon">
+                            <ClosedCaptionIcon className="icon_dashboard" style={{ fontSize: 30 }} />
+                            <div className="dashboard_sidebar_main_white">wipro34</div>
                         </div>
-                        <div className="sidebar_main_item sidebar_main_grey">Headth Studio</div>
-                        <div className="sidebar_main_item sidebar_main_icon">
-                            <AppsIcon className="icon_dashboard" style={{fontSize:30}}/>
-                            <div className="sidebar_main_white">Dashboard</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_grey">Resource Links</div>
+                        <div className="dashboard_sidebar_main_item dashboard_sidebar_main_icon">
+                            <ContactSupportIcon className="icon_dashboard" style={{ fontSize: 30 }} />
+                            <div className="dashboard_sidebar_main_white">Contact us</div>
                         </div>
                     </div>
-                    <div className="sidebar_main_footer">
-                        @2021 copywrite reserved to headth corporations
+                    <div className="dashboard_sidebar_main_footer">
+                        2021 copywrite reserved to headth corporations
                     </div>
                 </div>
             </div>
+            {/* dashboard sidebar ends here */}
             <div className="dashboard_content">
-                main content will be here
+                {/* main content 1 will start from here */}
+                <div className="main1">
+                    <div className="search_main">
+                        <input type="text" name="number" placeHolder="User Mobile Number" id="" className="search_main_number" />
+                        <div className="search_main_btn-container">
+                            <Button text="Search User" />
+                        </div>
+                    </div>
+                    <div className="recent_searches">
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recent_searches_user">
+                            <div className="recent_searches_user_part1">
+                                    <img src={process.env.PUBLIC_URL + 'assets/background_dashy.jpg'} alt="user logo" className="recent_searches_user_logo" />                                
+                            </div>
+                            <div className="recent_searches_user_text">
+                                <div className="recent_searches_user_text_part1">
+                                    <div className="recent_searches_name">Manjeet Singh</div>
+                                    <div className="recent_searches_btn">View</div>
+                                </div>
+                                <div className="recent_searches_user_text_part2">
+                                    <div className="recent_searches_document">44 Documents</div>
+                                    <div className="recent_searches_dob">23 / 02 / 1999</div>
+                                </div>
+                                <div className="recent_searches_user_text_part3">
+                                    <LocationOnIcon fontSize="40" className="recent_searches_icon" />
+                                    <div className="recent_searches_location">Bhopal,Madhya Pradesh</div>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+                <div className="main2">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex quidem quibusdam temporibus, fugit a similique libero consequuntur possimus dolorem! Est id ab maxime quo rerum saepe dolore alias provident iusto.
+                </div>
             </div>
+        </div>
         </div>
     )
 }
