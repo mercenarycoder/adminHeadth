5b9a3b4fb1f63edc07e66bd13b576c9b498eda68
Manjeet <manjeetts869@gmail.com>
2021-07-18 14:44:15 +0530

    profile section made

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -392,7 +392,75 @@ export default function Dashboard() {
                                 </li>
                             </ul>
                         </div>
+                        <div className="reports">
+                            <div className="report">
+                                <div className="report_name">
+                                    Report Main
+                                </div>
+                                <div className="report_date">
+                                    10-07-2021
+                                </div>
+                                <div className="report_type">
+                                    Dental
+                                </div>
+                                <div className="report_doctor">
+                                    Dr. Manjeet Singh
+                                </div>
+                                <div className="report_logo">
+                                    <img src={process.env.PUBLIC_URL + 'assets/3dlogo.png'} alt="report logo" className="report_icon"/>
+                                </div>
+                                <div className="report_view">
+                                    <div className="report_viewer">
+                                        View
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="report">
+                                <div className="report_name">
+                                    Report Main
+                                </div>
+                                <div className="report_date">
+                                    10-07-2021
+                                </div>
+                                <div className="report_type">
+                                    Dental
+                                </div>
+                                <div className="report_doctor">
+                                    Dr. Manjeet Singh
+                                </div>
+                                <div className="report_logo">
+                                    <img src={process.env.PUBLIC_URL + 'assets/3dlogo.png'} alt="report logo" className="report_icon"/>
+                                </div>
+                                <div className="report_view">
+                                    <div className="report_viewer">
+                                        View
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="report">
+                                <div className="report_name">
+                                    Report Main
+                                </div>
+                                <div className="report_date">
+                                    10-07-2021
+                                </div>
+                                <div className="report_type">
+                                    Dental
+                                </div>
+                                <div className="report_doctor">
+                                    Dr. Manjeet Singh
+                                </div>
+                                <div className="report_logo">
+                                    <img src={process.env.PUBLIC_URL + 'assets/3dlogo.png'} alt="report logo" className="report_icon"/>
+                                </div>
+                                <div className="report_view">
+                                    <div className="report_viewer">
+                                        View
+                                    </div>
+                                </div>
+                            </div>
                         
+                        </div>
                     </div>
                 </div>
             </div>
